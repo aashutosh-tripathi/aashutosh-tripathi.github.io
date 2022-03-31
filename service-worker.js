f351268d915da4d00e726dd0b2b040fdf5f136e2
Aashutosh Tripathi <aashutosh.tripathi24@gmail.com>
2022-03-31 17:02:19 +0530

    Fixed college logo and infosys logo

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e0ee2707f0b6ee8d31cc4cd7ac9c7461.js"
+  "/precache-manifest.16e6c55bc4033692f9afdc9332698dee.js"
 );
 
 self.addEventListener('message', (event) => {
